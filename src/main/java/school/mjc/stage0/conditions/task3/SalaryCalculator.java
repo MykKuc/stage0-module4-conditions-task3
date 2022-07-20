aef5410ca068a753a73078997aa6172f1897014f
Mykoliux <mykolas.kucinskas2@gmail.com>
2022-07-20 15:29:37 +0300

    Tasks completed.

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double calculatedSalary;
+        if(salary < 0 ){
+            System.out.println("wrong input!");
+        }else if(salary <= 10000){
+            calculatedSalary = salary*0.85;
+            System.out.println(calculatedSalary);
+        }else if(salary <= 20000){
+            calculatedSalary = salary*0.82;
+            System.out.println(calculatedSalary);
+        }else{
+            calculatedSalary = salary*0.80;
+            System.out.println(calculatedSalary);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,6 +2,21 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
+        if (month <= 0){
+            System.out.println("wrong number!");
+        }else if(month <= 2){
+            System.out.println("Winter");
+        }else if(month <= 5){
+            System.out.println("Spring");
+        }else if(month <= 8){
+            System.out.println("Summer");
+        }else if(month <= 11){
+            System.out.println("Autumn");
+        }else if(month == 12){
+            System.out.println("Winter");
+        }else if(month > 12){
+            System.out.println("wrong number!");
+        }
 
     }
 }
